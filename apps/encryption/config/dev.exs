b749a03188e14b181ca48f5e079daa4ecf0de7cc
Sam Schneider <sschneider1207@gmail.com>
2016-08-02 01:10:27 +0000

    update encryption module

--- apps/encryption/config/dev.exs
+++ apps/encryption/config/dev.exs
@@ -1,5 +1,5 @@
 use Mix.Config
 
 config :geosnap_encryption,
-  public_key: File.read!("priv/dev.pub"),
-  private_key: File.read!("priv/dev")
+  public_key: "BA7+MZK0Y3sEct/YclMnSdcFbTpR/BlzG6cE388TtTrqJv6YwQSvmVZNTvh0oR+5DgpXxChRlEXu+ltqe3U9J8Rf9KvELSxC12xwvj7A9P7VIzjHQMVeMUZcWAO20E1vNsyFAf3odeqI6QChln9o0t27otxeagmBT4daJ4xKBQYC",
+  private_key: "kKs8eL+J+lGYp8tLgMkQHHS7fYRyPw8hLtNe7DEJ+XgEkVlNZGzhKJw7B/FOgvseEHVlnFEjZs7Isv6/l1Dwag=="
--- apps/encryption/lib/geosnap/encryption.ex
+++ apps/encryption/lib/geosnap/encryption.ex
@@ -1,11 +1,28 @@
 defmodule Geosnap.Encryption do
   @curve Application.get_env(:geosnap_encryption, :curve_name)
+  @private_key Application.get_env(:geosnap_encryption, :private_key)
 
+  @type key :: {public_key, private_key}
+  @type public_key :: String.t
+  @type private_key :: String.t
+  
+  @doc """
+  Generates an ECDH public/private key derived from the applications set private key.
+  """
+  @spec generate_key() :: key
   def generate_key do
+    generate_key(@private_key)
+  end
+
+  @doc """
+  Generates an ECDH public/private key derived from the specified private key, or from
+  none of not given.
+  """
+  @spec generate_key(nil | private_key) :: key
+  def generate_key(nil) do
     {pub, priv} = :crypto.generate_key(:ecdh, @curve)
     {Base.encode64(pub), Base.encode64(priv)}
   end
-
   def generate_key(priv) do
     {pub, priv} = :crypto.generate_key(:ecdh, @curve, priv)
     {Base.encode64(pub), Base.encode64(priv)}
