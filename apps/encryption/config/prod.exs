53ec3691ae46e0c84a89343e803df9e46b3a25f3
Sam Schneider <sschneider1207@gmail.com>
2016-08-02 03:50:20 +0000

    update encryption for other environment keys

--- apps/encryption/config/prod.exs
+++ apps/encryption/config/prod.exs
@@ -1,1 +1,6 @@
 use Mix.Config
+
+config :geosnap_encryption,
+  public_key: "BGBVQgFursnaEU/SYUwfdpzHr/81IiZ7f53FCO11o/RmOARdxjT/Awc6UiMy7p2o7WQF/qB+o5zJXxtG8J8UMy+hgP9EBfG6QKYiAR0LyVVHvLnHh4XqYgOiGP0FvYsRW6AYMO7I0thw/x/n7AVhyFc40cGE9g7XGVQ3LuLI9Qqy"
+
+import_config "#{Mix.env}.secret.exs"
--- apps/encryption/config/test.exs
+++ apps/encryption/config/test.exs
@@ -1,1 +1,5 @@
 use Mix.Config
+
+config :geosnap_encryption,
+  public_key: "BA6PzDUJajuku2Wj3e5m+G2K1WR9NMrXWI0c8kukbgvsVDcxZIn4ySu5vXGtBL3iQmw8I0SfBOBAvV97KhXBvEp6aTa0/SWYqhwFS6VVxuGbk29LkFNm3eizCfo5RZgpg3W31yIRgpTyjRyFPDbAlzHQ7toi0t1Ybepb7qb4zXzG",
+  private_key: "bMT7yW0Z68mwARUapN7O8vmF47kAdn5FzrqdLbXyMGtBRpneTfQq/C3fcY82uhJt3lt95xcGxQanlKaf7+H+jw=="
