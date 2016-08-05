b690a2c885f100bdb3d3fe4ba00f9bdf9be5271c
Sam Schneider <sschneider1207@gmail.com>
2016-08-05 01:37:15 +0000

    more changesets for users

--- apps/db/lib/geosnap/db/user.ex
+++ apps/db/lib/geosnap/db/user.ex
@@ -2,12 +2,16 @@ defmodule Geosnap.Db.User do
   use Ecto.Schema
   alias Geosnap.Db.{Picture, PictureVote, Comment, CommentVote}
   use Geosnap.Db.Changeset
+  alias Ecto.Changeset
+
+  @type t :: %__MODULE__{}
 
   schema "users" do
     field :username, :string
     field :email, :string
     field :password, :string, virtual: true
     field :hashed_password, :string
+    field :verified_email, :boolean, default: false
     field :permissions, :integer, default: 0
     
     has_many :pictures, Picture
@@ -21,21 +25,63 @@ defmodule Geosnap.Db.User do
   @doc """
   Creates a changeset for a new user based on a set of params.
   """
-  @spec new_changeset(map) :: Ecto.Changeset.t
+  @spec new_changeset(map) :: Changeset.t
   def new_changeset(params) do
     %__MODULE__{}
-    |> changeset(params)
-    |> validate_required(:password)
+    |> cast(params, [:username, :email, :password, :permissions])
+    |> validate_required([:username, :email, :password])
+    |> validate_email(:email)
     |> validate_length(:password, min: 10)
     |> hash_password_field()
-  end
-
-  defp changeset(user, params) do
-    user
-    |> cast(params, [:username, :email, :password, :permissions])
-    |> validate_required([:username, :email])
-    |> validate_email(:email)
+    |> validate_number(:permissions, greater_than_or_equal_to: 0)
     |> unique_constraint(:username)
     |> unique_constraint(:email)
-  end 
+  end
+
+  @doc """
+  Creates a changeset that marks a user's email as verified. 
+  """
+  @spec verify_email_changeset(t) :: Changeset.t
+  def verify_email_changeset(user) do
+    user
+    |> cast(%{verified_email: true}, [:verified_email])
+  end
+
+  @doc """
+  Creates a changeset for changing a user's password.
+  A confirmation parameter `:password_confirmation` is expected.
+  """
+  @spec change_password_changeset(t, map) :: Changeset.t
+  def change_password_changeset(user, confirmed_pwd) do
+    user
+    |> cast(confirmed_pwd, [:password])
+    |> validate_required([:password])
+    |> validate_confirmation(:password, required: true)
+    |> hash_password_field()
+  end
+  
+  @doc """
+  Creates a changeset for changing a user's email.
+  A confirmation parameter `:email_confirmation` is expected.
+  """
+  @spec change_email_changeset(t, map) :: Changeset.t
+  def change_email_changeset(user, confirmed_email) do
+    params = Map.put(confirmed_email, :verified_email, false)
+    user
+    |> cast(params, [:email, :verified_email])
+    |> validate_required([:email])
+    |> validate_confirmation(:email, required: true)
+    |> validate_email(:email)
+  end
+
+  @doc """
+  Creates a changeset for updating a user's permissions.
+  """
+  @spec update_permissions(t, integer) :: Changeset.t
+  def update_permissions(user, permissions) do
+    user
+    |> cast(%{permissions: permissions}, [:permissions])
+    |> validate_required(:permissions)
+    |> validate_number(:permissions, greater_than_or_equal_to: 0)
+  end
 end
--- apps/db/test/geosnap/db/user_test.exs
+++ apps/db/test/geosnap/db/user_test.exs
@@ -1,6 +1,7 @@
 defmodule Geosnap.Db.UserTest do
   use ExUnit.Case
   alias Geosnap.Db.{User, Repo}
+  alias Geosnap.Encryption
 
   setup do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
@@ -9,7 +10,7 @@ defmodule Geosnap.Db.UserTest do
   test "new changeset hashes password correctly" do
     params = %{username: "username", password: "password123", email: "email@test.com"}
     changeset = User.new_changeset(params)
-    match = Geosnap.Encryption.check_password("password123", changeset.changes.hashed_password)
+    match = Encryption.check_password("password123", changeset.changes.hashed_password)
     
     assert match == true
   end
@@ -30,43 +31,104 @@ defmodule Geosnap.Db.UserTest do
     assert changeset.valid? == false
   end
 
-  test "valid new user inserts ok" do
-    params = %{username: "username", password: "password123", email: "email@test.com"}
-    {:ok, user} =
-      User.new_changeset(params)
-      |> Repo.insert()
-
-    assert user.username == "username"
-    assert user.email == "email@test.com"
+  test "new user starts with unverified email" do
+    {:ok, user} = new_user()
+    
+    assert user.verified_email == false
   end
 
   test "cannot insert duplicate username" do
-    params1 = %{username: "username", password: "password123", email: "email@test.com"}
-    {:ok, user} =
-      User.new_changeset(params1)
-      |> Repo.insert()
-
-    params2 = %{username: user.username, password: "password123", email: "email2@test.com"}
-    {:error, changeset} =
-      User.new_changeset(params2)
-      |> Repo.insert()
+    {:ok, user} = new_user()
+    params = %{username: user.username, password: "password123", email: "email2@test.com"}
+    {:error, changeset} = new_user(params)
 
     assert changeset.valid? == false
     assert changeset.errors[:username] == {"has already been taken", []}
   end
 
   test "cannot insert duplicate email" do
-    params1 = %{username: "username", password: "password123", email: "email@test.com"}
-    {:ok, user} =
-      User.new_changeset(params1)
-      |> Repo.insert()
-    
-    params2 = %{username: "username1", password: "password123", email: user.email}
-    {:error, changeset} =
-      User.new_changeset(params2)
-      |> Repo.insert()
+    {:ok, user} = new_user()
+    params = %{username: "username1", password: "password123", email: user.email}
+    {:error, changeset} = new_user(params)
 
     assert changeset.valid? == false
     assert changeset.errors[:email] == {"has already been taken", []}
+  end
+
+  test "verify user changeset is valid" do
+    {:ok, user} = new_user()
+    changeset = User.verify_email_changeset(user)
+
+    assert changeset.valid? == true
+  end
+
+  test "verify confirmed change password changeset is valid" do
+    {:ok, user} = new_user()
+    params = %{password: "password321", password_confirmation: "password321"}
+    changeset = User.change_password_changeset(user, params) 
+    
+    assert changeset.valid? == true
+    match = Encryption.check_password("password321", changeset.changes.hashed_password)
+    assert match == true
+  end
+
+  test "verify unconfirmed change password changeset isn't valid" do
+    {:ok, user} = new_user()
+    params = %{password: "password321", password_confirmation: "password123"}
+    changeset = User.change_password_changeset(user, params) 
+    
+    assert changeset.valid? == false
+  end
+
+  test "verify confirmed change email changeset is valid" do
+    {:ok, user} = new_user()
+    {:ok, verified_user} = 
+      User.verify_email_changeset(user)
+      |> Repo.update()
+    params = %{email: "email2@test.com", email_confirmation: "email2@test.com"}
+    changeset = User.change_email_changeset(verified_user, params) 
+    
+    assert changeset.valid? == true
+    assert changeset.changes.verified_email == false
+  end
+
+  test "verify unconfirmed change email changeset isn't valid" do
+    {:ok, user} = new_user()
+    params = %{email: "email2@test.com", email_confirmation: "email@test.com"}
+    changeset = User.change_email_changeset(user, params) 
+    
+    assert changeset.valid? == false
+  end
+
+  test "update permissions changeset isn't valid with negative number" do
+    {:ok, user} = new_user()
+    changeset = User.update_permissions(user, -1)
+
+    assert changeset.valid? == false
+  end
+
+  test "update permissions changeset is valid with positive number" do
+    {:ok, user} = new_user()
+    changeset = User.update_permissions(user, 4)
+
+    assert changeset.valid? == true
+  end
+
+  test "update permissions changeset isn't valid with nil" do
+    {:ok, user} = new_user()
+    changeset = User.update_permissions(user, nil)
+
+    assert changeset.valid? == false
+  end
+
+  defp new_user(params \\ %{}) do
+    %{
+      username: "username",
+      password: "password123",
+      email: "email@test.com"
+    } 
+    |> Map.merge(params)
+    |> User.new_changeset()
+    |> Repo.insert()
   end
 end
