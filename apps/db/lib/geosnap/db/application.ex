b155368d3bfa0eb0348c90dd995f7c10ac4051ae
Sam Schneider <sschneider1207@gmail.com>
2016-08-05 02:02:48 +0000

    added more application changesets

--- apps/db/lib/geosnap/db/application.ex
+++ apps/db/lib/geosnap/db/application.ex
@@ -1,10 +1,14 @@
 defmodule Geosnap.Db.Application do
   use Ecto.Schema
   use Geosnap.Db.Changeset
+  alias Ecto.Changeset
+
+  @type t :: %__MODULE__{}
 
   schema "applications" do
     field :name, :string
     field :email, :string
+    field :verified_email, :boolean, default: false
 
     has_one :api_key, Geosnap.Db.ApiKey
 
@@ -14,10 +18,32 @@ defmodule Geosnap.Db.Application do
   @doc """
   Creates a changeset for a new application based on a set of params.
   """
-  @spec new_changeset(map) :: Ecto.Changeset.t
+  @spec new_changeset(map) :: Changeset.t
   def new_changeset(params) do
     %__MODULE__{}
     |> cast(params, [:name, :email])
+    |> validate_email(:email)
+  end
+
+  @doc """
+  Creates a changeset for verifying an application's email.
+  """
+  @spec verify_email(t) :: Changeset.t
+  def verify_email(application) do
+    application
+    |> cast(%{verified_email: true}, [:verified_email])
+  end
+
+  @doc """
+  Creates a changeset for changing an application's email.
+  """
+  @spec change_email_changeset(t, map) :: Changeset.t
+  def change_email_changeset(application, confirmed_email) do
+    params = Map.put(confirmed_email, :verified_email, :false)
+    application
+    |> cast(params, [:email, :verified_email])
+    |> validate_required([:email])
+    |> validate_confirmation(:email, required: true)
     |> validate_email(:email)
   end
 end
--- apps/db/test/geosnap/db/application_test.exs
+++ apps/db/test/geosnap/db/application_test.exs
@@ -1,6 +1,10 @@
 defmodule Geosnap.Db.ApplicationTest do
   use ExUnit.Case, async: true
-  alias Geosnap.Db.Application
+  alias Geosnap.Db.{Application, Repo}
+
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+  end
 
   test "valid new changeset" do
     params = %{name: "test_name", email: "email@test.com"}
@@ -12,5 +16,43 @@ defmodule Geosnap.Db.ApplicationTest do
     params = %{name: "test_name", email: "email@te st.com"}
     changeset = Application.new_changeset(params)
     assert changeset.valid? == false
+  end
+
+  test "verify email changeset correctly flips status" do
+    {:ok, app} = new_application()
+    changeset = Application.verify_email(app)
+
+    assert changeset.valid? == true
+    assert changeset.changes.verified_email == true
+  end
+
+  test "verify confirmed change email changeset is valid" do
+    {:ok, app} = new_application()
+    {:ok, verified_app} = 
+      Application.verify_email(app)
+      |> Repo.update()
+    params = %{email: "email2@test.com", email_confirmation: "email2@test.com"}
+    changeset = Application.change_email_changeset(verified_app, params)
+
+    assert changeset.valid? == true
+    assert changeset.changes.verified_email == false
+  end
+
+  test "verify unconfirmed change email changeset isn't valid" do
+    {:ok, app} = new_application()
+    params = %{email: "email2@test.com", email_confirmation: "email@test.com"}
+    changeset = Application.change_email_changeset(app, params)
+
+    assert changeset.valid? == false
+  end
+
+  defp new_application(params \\ %{}) do
+    %{
+      name: "test_name",
+      email: "email@test.com"
+    }
+    |> Map.merge(params)
+    |> Application.new_changeset()
+    |> Repo.insert()
   end
 end
