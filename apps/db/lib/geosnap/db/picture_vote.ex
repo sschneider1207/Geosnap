661d15b1db5b3331fe7a374740c6d0bdd88ff831
Sam Schneider <sschneider1207@gmail.com>
2016-08-07 14:38:46 +0000

    picture vote changesets and test

--- apps/db/lib/geosnap/db/picture_vote.ex
+++ apps/db/lib/geosnap/db/picture_vote.ex
@@ -1,6 +1,10 @@
 defmodule Geosnap.Db.PictureVote do
   use Geosnap.Db.Schema
+  use Geosnap.Db.Changeset
   alias Geosnap.Db.{User, Picture}
+  alias Ecto.Changeset
+
+  @type t :: %__MODULE__{}
 
   schema "picture_votes" do
     field :value, :integer
@@ -9,6 +13,30 @@ defmodule Geosnap.Db.PictureVote do
     belongs_to :picture, Picture
 
     timestamps
+  end
 
+  @doc """
+  Creates a new changeset for a user's vote for a given picture.
+  """
+  @spec new_changeset(map) :: Changeset.t
+  def new_changeset(params) do
+    %__MODULE__{}
+    |> cast(params, [:user_id, :picture_id, :value])
+    |> validate_required([:user_id, :picture_id, :value])
+    |> validate_number(:value, [greater_than_or_equal_to: -1, less_than_or_equal_to: 1])
+    |> assoc_constraint(:user)
+    |> assoc_constraint(:picture)
+    |> unique_constraint(:picture, name: :picture_votes_user_id_picture_id_index)
+  end
+
+  @doc """
+  Creates a changeset for updating the value of an existing vote.
+  """
+  @spec update_vote_changeset(t, integer) :: Changeset.t
+  def update_vote_changeset(vote, new_value) do
+    vote
+    |> cast(%{value: new_value}, [:value])
+    |> validate_required([:value])
+    |> validate_number(:value, [greater_than_or_equal_to: -1, less_than_or_equal_to: 1])
   end
 end
---
+++ apps/db/test/geosnap/db/picture_vote_test.exs
@@ -0,0 +1,105 @@
+defmodule Geosnap.Db.PictureVoteTest do
+  use ExUnit.Case, async: true
+  alias Geosnap.Db.{PictureVote, User, Picture, Category, Repo}
+
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+  end
+
+  test "value must not be greater than 1 for new changeset to be valid" do
+    changeset = PictureVote.new_changeset(%{user_id: 1, picture_id: 1, value: 2})
+
+    assert changeset.valid? == false
+  end
+
+  test "value must not be less than -1 for new changeset to be valid" do
+    changeset = PictureVote.new_changeset(%{user_id: 1, picture_id: 1, value: -2})
+
+    assert changeset.valid? == false
+  end
+
+  test "picture must exist for new changeset to be valid" do
+    {:ok, user} = 
+      %{username: "user", password: "1234567890", email: "e@t.com"}
+      |> User.new_changeset()
+      |> Repo.insert()
+
+    {:error, changeset} =
+      %{user_id: user.id, picture_id: 1, value: 1}
+      |> PictureVote.new_changeset()
+      |> Repo.insert()
+
+    assert changeset.valid? == false
+  end
+
+  test "user must exist for new changeset to be valid" do
+    {:ok, category} =
+      Category.new_changeset("cat")
+      |> Repo.insert()
+
+    {:ok, user} = 
+      %{username: "user", password: "1234567890", email: "e@t.com"}
+      |> User.new_changeset()
+      |> Repo.insert()
+
+    {:ok, picture} =
+      picture_params(user.id, category.id)
+      |> Picture.new_changeset()
+      |> Repo.insert()
+
+    {:error, changeset} =
+      %{user_id: user.id + 1, picture_id: picture.id, value: 1}
+      |> PictureVote.new_changeset()
+      |> Repo.insert()
+
+    assert changeset.valid? == false
+  end
+
+  test "valid new vote inserts okay" do
+    {:ok, category} =
+      Category.new_changeset("cat")
+      |> Repo.insert()
+
+    {:ok, user} = 
+      %{username: "user", password: "1234567890", email: "e@t.com"}
+      |> User.new_changeset()
+      |> Repo.insert()
+
+    {:ok, picture} =
+      picture_params(user.id, category.id)
+      |> Picture.new_changeset()
+      |> Repo.insert()
+
+    result =
+      %{user_id: user.id, picture_id: picture.id, value: 1}
+      |> PictureVote.new_changeset()
+      |> Repo.insert()
+
+    assert {:ok, _vote} = result
+  end
+
+  test "value must not be greater than 1 for update changeset to be valid" do
+    changeset = PictureVote.update_vote_changeset(%PictureVote{}, 2)
+
+    assert changeset.valid? == false
+  end
+
+  test "value must not be less than -1 for update changeset to be valid" do
+    changeset = PictureVote.update_vote_changeset(%PictureVote{}, -2) 
+
+    assert changeset.valid? == false
+  end
+
+  defp picture_params(user_id, category_id) do
+    %{
+      title: "title",
+      location: %Geo.Point{coordinates: {0,0}, srid: 4326},
+      expiration: Timex.now() |> Timex.shift([hours: 1]),
+      md5: "abc",
+      picture_path: "/p/t/p",
+      thumbnail_path: "/p/t/t",
+      user_id: user_id,
+      category_id: category_id
+    }
+  end
+end
