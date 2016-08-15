f39af8de04dd6b199cb0696c35018c33f799d678
Sam Schneider <sschneider1207@gmail.com>
2016-08-15 18:41:42 -0400

    user functions plus missed an application one

--- apps/db/lib/geosnap/db.ex
+++ apps/db/lib/geosnap/db.ex
@@ -1,5 +1,5 @@
 defmodule Geosnap.Db do
-  alias Geosnap.Db.{Repo, Application, ApiKey}
+  alias Geosnap.Db.{Repo, Application, ApiKey, User}
   import Ecto.Query
 
   @doc """
@@ -19,6 +19,17 @@ defmodule Geosnap.Db do
       changeset ->
         {:error, errors_to_map(changeset)}
     end
+  end
+
+  @doc """
+  Changes the email for a user.
+  """
+  @spec change_application_email(Application.t, map) :: {:ok, Application.t} | {:error, map}
+  def change_application_email(application, params) do
+    insert_or_update(
+      fn -> Application.change_email_changeset(application, params) end,
+      &Repo.update/1
+    )
   end
 
   @doc """
@@ -52,10 +63,66 @@ defmodule Geosnap.Db do
     end
   end
 
+  @doc """
+  Creates a new user.
+  """
+  @spec new_user(map) :: {:ok, User.t} | {:error, map}
+  def new_user(params) do
+    insert_or_update(
+      fn -> User.new_changeset(params) end,
+      &Repo.insert/1
+    )
+  end
+
+  @doc """
+  Changes the password for a user.
+  """
+  @spec change_user_password(User.t, map) :: {:ok, User.t} | {:error, map}
+  def change_user_password(user, params) do
+    insert_or_update(
+      fn -> User.change_password_changeset(user, params) end,
+      &Repo.update/1
+    )
+  end
+
+  @doc """
+  Changes the email for a user.
+  """
+  @spec change_user_email(User.t, map) :: {:ok, User.t} | {:error, map}
+  def change_user_email(user, params) do
+    insert_or_update(
+      fn -> User.change_email_changeset(user, params) end,
+      &Repo.update/1
+    )
+  end
+
+  @doc """
+  Get a user by either user id or username.
+  """
+  @spec get_user(integer | String.t) :: User.t | nil
+  def get_user(user_id) when is_integer(user_id) do
+    Repo.get(User, user_id)
+  end
+  def get_user(username) do
+    Repo.get_by(User, username: username)
+  end
+
+  defp insert_or_update(gen_changeset, repo_action) do
+    with %{valid?: true} = changeset <- gen_changeset.(),
+      {:ok, schema} <- repo_action.(changeset) do
+        {:ok, schema}
+    else
+      {:error, changeset} ->
+        {:error, errors_to_map(changeset)}
+      changeset ->
+        {:error, errors_to_map(changeset)}
+    end
+  end
+
   defp errors_to_map(changeset) do
     Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
       Enum.reduce(opts, msg, fn {key, value}, acc ->
-        String.replace(msg, "%{#{key}}", to_string(value))
+        String.replace(acc, "%{#{key}}", to_string(value))
       end)
     end)
   end
