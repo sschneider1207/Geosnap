844510135ed3c3f02b24369a7d1551b3086176e3
Sam Schneider <sschneider1207@gmail.com>
2017-01-14 21:43:04 -0500

    missed adding

---
+++ apps/identity/test/identity/user_supervisor_test.exs
@@ -0,0 +1,24 @@
+defmodule Identity.UserSupervisorTest do
+  use ExUnit.Case
+  alias Identity.{User, UserSupervisor, UserRegistry, Repo}
+
+  test "can start a registered process with a user struct" do
+    user = %User{id: 1}
+    {:ok, pid} = UserSupervisor.start_child(user)
+
+    assert {^pid, ^user} = UserRegistry.lookup(user.id)
+  end
+
+  test "can start a registered process with a set of params" do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+    Ecto.Adapters.SQL.Sandbox.mode(Identity.Repo, {:shared, self()})
+    params = %{username: "user", password: "password123", email: "email@nsa.gov"}
+    {:ok, pid} = UserSupervisor.start_child(params)
+
+    assert {^pid, _user} = UserRegistry.lookup(pid)
+  end
+
+  test "can't start with invalid parmas" do
+    assert {:error, _} = UserSupervisor.start_child(%{})
+  end
+end
