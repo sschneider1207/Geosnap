8bdd9aafbf75c1535227facf9bdeb6afb1b08502
Sam Schneider <sschneider1207@gmail.com>
2017-03-05 19:11:17 -0500

    table doc

--- apps/store_house/lib/store_house/table.ex
+++ apps/store_house/lib/store_house/table.ex
@@ -1,4 +1,24 @@
 defmodule StoreHouse.Table do
+  @moduledoc """
+  Convience macro for using mnesia records.  Records are defined in the
+  "src/records.hrl" file.
+
+  # Usage
+
+  Use this module in another one and pass the name of a record as the argument.
+
+      defmodule Foo do
+        use StoreHouse.Table, :foo
+        ...
+      end
+
+  In this case, the record for `:foo` is extracted from the record definitions
+  file and is passed to`Record.defrecord/2`/`Kernel.defstruct/1`.
+
+  In addition, a `row_to_struct/1` function is defined in order to convert
+  a mnesia record to a struct representation.
+
+  """
 
   defmacro __using__(name) do
     attrs = Record.extract(name, from: "src/records.hrl")
